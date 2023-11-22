4c10d3c71be246e78c245302d47301a6feb3d4c0
Abhishek Gupta <abhishek.ag700@gmail.com>
2023-11-22 22:36:12 +0530

    Create Challenge.java
    Each person in Italy has an unique identifying ID code issued by the national tax office after the birth registration: the Fiscal Code (Codice Fiscale). Check the Resources tab for more info on this.
    
    Given an object containing the personal data of a person (name, surname, gender and date of birth) return the 11 code characters as a string following these steps:
    
    Generate 3 capital letters from the surname, if it has:
    
    At least 3 consonants then the first three consonants are used. (Newman -> NWM).
    Less than 3 consonants then vowels will replace missing characters in the same order they appear (Fox -> FXO | Hope -> HPO).
    Less than three letters then "X" will take the third slot after the consonant and the vowel (Yu -> YUX).
    Generate 3 capital letters from the name, if it has:
    
    Exactly 3 consonants then consonants are used in the order they appear (Matt -> MTT).
    More than 3 consonants then first, third and fourth consonant are used (Samantha -> SNT | Thomas -> TMS).
    Less than 3 consonants then vowels will replace missing characters in the same order they appear (Bob -> BBO | Paula -> PLA).
    Less than three letters then "X" will take the the third slot after the consonant and the vowel (Al -> LAX).
    Generate 2 numbers, 1 letter and 2 numbers from date of birth and gender:
    
    Take the last two digits of the year of birth (1985 -> 85).
    Generate a letter corresponding to the month of birth (January -> A | December -> T) using the table for conversion included in the code.
    For males take the day of birth adding one zero at the start if is less than 10 (any 9th day -> 09 | any 20th day -> 20).
    For females take the day of birth and sum 40 to it (any 9th day -> 49 | any 20th day -> 60).

---
+++ Challenge.java
@@ -0,0 +1,98 @@
+public class Challenge {
+    static final String[] months = {"","A","B","C","D","E","H","L","M","P","R","S","T"};
+    public static String fiscalCode(String name, String surName, String gender, String dob) {
+        String code = null;
+        if(surName.length()<3){
+            code = consonant(surName)+vowel(surName) + "X";
+        }
+        else{
+            if (checkconsonant(surName)>=3){
+                code = consonant(surName).substring(0,3);
+            }
+            else {
+                code = consonant(surName) + vowel(surName).charAt(0);
+            }
+        }
+        if(name.length()<3){
+            code = code + consonant(name)+vowel(name) + "X";
+        }
+        else{
+            if (checkconsonant(name)>3){
+                code = code + consonant(name).charAt(0)+ consonant(name).substring(2,4);
+            }
+            else if(checkconsonant(name)==3){
+                code = code + consonant(name);
+            }
+            else{
+                code = code + consonant(name) + vowel(name).charAt(0);
+            }
+        }
+        code =code + dob.substring(dob.length()-2,dob.length());
+        code = code + months[Integer.parseInt(dob.substring(dob.indexOf('/')+1,dob.lastIndexOf('/')))];
+        int date = Integer.parseInt(dob.substring(0,dob.indexOf('/')));
+        if(gender.charAt(0) == 'M'){
+            if (date > 0 && date <10){
+                code = code + "0" + date;
+            }
+            else{
+                code = code + date;
+            }
+        }
+        else {
+            if (date > 0 && date <10){
+                code = code + (40 + date);
+            }
+            else{
+                code = code + (40 + date);
+            }
+
+        }
+        return code;
+    }
+    public static String consonant(String str){
+        str = str.toUpperCase();
+        String OnlyConsonant = "";
+        int i = 0,j = 0;
+        while (i < str.length()){
+            if(str.charAt(i)!='A' && str.charAt(i)!='E' && str.charAt(i)!='I' && str.charAt(i)!='O' && str.charAt(i)!='U'){
+                OnlyConsonant = OnlyConsonant.concat(String.valueOf(str.charAt(i)));
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+        return OnlyConsonant;
+    }
+    public static String vowel(String str){
+        str = str.toUpperCase();
+        String OnlyVowel = "";
+        int i = 0,j = 0;
+        while (i < str.length()){
+            if(str.charAt(i)=='A' || str.charAt(i)=='E' || str.charAt(i)=='I' || str.charAt(i)=='O' || str.charAt(i)=='U'){
+                OnlyVowel = OnlyVowel.concat(String.valueOf(str.charAt(i)));
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+        return OnlyVowel;
+    }
+    public static int checkconsonant(String str){
+        str = str.toUpperCase();
+        int i = 0,j=0;
+        while (i < str.length()){
+            if(str.charAt(i)!='A' && str.charAt(i)!='E' && str.charAt(i)!='I' && str.charAt(i)!='O' && str.charAt(i)!='U'){
+                j++;
+                i++;
+            }
+            else{
+                i++;
+            }
+        }
+        return j;
+    }
+
+
+}
